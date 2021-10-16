415ffdffa2a558f87c1162ff23f3c3b3e74a60c5
fajry hamzah <fhaji.jakmall@gmail.com>
2021-10-16 14:23:26 +0700

    renaming file to follow google naming convention

--- src/utils/whitelist_user.ts
+++ src/utils/whitelist_user.ts
@@ -1,10 +1,10 @@
 'use strict';
-import Config from '../app-config';
+import Config from '../app_config';
 
 const config = Config.getConfig();
 
 export default (username:string) => {
     let whitelistUser:string[] = config.getWhitelistUsername();
-    console.log(whitelistUser);
+
     return whitelistUser.indexOf(username) > -1;
 }
