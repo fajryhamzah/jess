c63d82cc904c1a8e837f436f38ee2d664a5804fb
fajry hamzah <fhaji.jakmall@gmail.com>
2021-10-18 21:01:18 +0700

    add cron for every 5 minute

--- src/event/on_ready_event.ts
+++ src/event/on_ready_event.ts
@@ -1,5 +1,9 @@
-import { Client, Permissions } from "discord.js";
-import * as notifyRepo from '../notif_channel/repository'; 
+import { Channel, Client, GuildChannel, Permissions, TextChannel, ThreadChannel } from "discord.js";
+import * as notifyRepo from '../notif_channel/repository';
+import cron from 'cron';
+import * as coinRepo from '../coin_list/repository';
+import { LiveCoinWatch } from 'livecoinwatch-wrapper-api';
+import Config from '../app_config';
 
 const isChannelShouldBeNotify = async (channelId: string): Promise<boolean> => {
     const channel = await notifyRepo.findByChannelId(channelId);
@@ -7,29 +11,61 @@ const isChannelShouldBeNotify = async (channelId: string): Promise<boolean> => {
     return channel.length > 0;
 }
 
+const livecoin = new LiveCoinWatch(Config.getConfig().getLiveAPIToken());
+
+const formatUSD = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
+const notifier = async (channel: ThreadChannel) => {
+    const allCoin = await coinRepo.getAllCoin();
+
+    for (let coin of allCoin) {
+        livecoin.getCoin('USD', coin.coin_code, false).then(response => {
+            const price = <number>coin.price;
+            const thresholdType = <number>coin.threshold_type;
+            const coinCode = <string>coin.coin_code.toUpperCase();
+
+            switch (thresholdType) {
+                case coinRepo.THRESHOLD_TYPE_UNDER:
+                    response.rate < price ? channel.send(coinCode + ' is under ' + formatUSD.format(coin.price) + '. Real Price : ' + formatUSD.format(response.rate)) : null;
+                    break;
+                case coinRepo.THRESHOLD_TYPE_UPPER:
+                    response.rate > price ? channel.send(coinCode + ' is more than ' + formatUSD.format(coin.price) + '. Real Price : ' + formatUSD.format(response.rate)) : null;
+                    break;
+            }
+        }).catch(e => console.log('something happer with api', e));
+    }
+}
+
+
 const event = (client: Client) => {
     console.log(`There is a new girl in town, an it\`s Jess!`);
-    let jessId = client.user?.id || '';
 
-    client.guilds.cache.map(guild => {
-        let jessMemberGuild = guild.members.resolveId(jessId);
+    const job = new cron.CronJob('*/5 * * * *', () => {
+        let jessId = client.user?.id || '';
 
-        guild.channels.cache.map(async (channel) => {
-            if (!channel.isText() || !channel.permissionsFor(jessMemberGuild)?.has([Permissions.FLAGS.SEND_MESSAGES])) {
-                return;
-            }
+        client.guilds.cache.map(guild => {
+            let jessMemberGuild = guild.members.resolveId(jessId);
 
-            isChannelShouldBeNotify(channel.id);
-
-            try {
-                if (await isChannelShouldBeNotify(channel.id)) {
-                    await channel.send('It\' Jess');
+            guild.channels.cache.map(async (channel) => {
+                if (!channel.isText() || !channel.permissionsFor(jessMemberGuild)?.has([Permissions.FLAGS.SEND_MESSAGES])) {
+                    return;
                 }
-            } catch (e) {
-                console.log('Error on sending message');
-            }
-        })
-    })
+
+                try {
+                    if (await isChannelShouldBeNotify(channel.id)) {
+                        notifier(channel);
+                    }
+                } catch (e) {
+                    console.log('Error on sending message');
+                }
+            })
+        });
+    });
+
+    job.start();
 }
 
 export default event;
